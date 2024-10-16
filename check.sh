72503b266d9bbc13aa153d75f060a15aa3d487ce
Shamit Soni <shamit.soni4@gmail.com>
2024-10-16 21:15:26 +0000

    Retry connection
    Add a simple check to retry a connection to a website with a specified number of attempts before marking the website as offline in the console and log.

--- check.sh
+++ check.sh
@@ -4,11 +4,21 @@ LOG="status.log"
 TIME=$(date +"%Y-%m-%d %T")
 
 while IFS=" " read -r NAME SITE; do
-	if curl --output /dev/null --silent $SITE; then
-		echo "$TIME: $NAME is Operational" >> "$LOG"
-		echo "$NAME ----- Operational"
-	else
-		echo "$TIME: $NAME is Offline" >> "$LOG"
+	attempts=3
+	while [ $attempts -gt 0 ]; do	
+		if curl --output /dev/null --silent $SITE; then
+			echo "$TIME: $NAME is Operational" >> "$LOG"
+			echo "$NAME ----- Operational"
+			break
+		else
+			attempts=$((attempts-1))
+			echo "Retrying to connect to $NAME... [$attempts tries left]"
+			sleep 5
+		fi
+	done
+
+	if [ $attempts -eq 0 ]; then
+		echo "$TIME: $NAME is offline" >> "$LOG"
 		echo "$NAME ----- Offline"
 	fi
 done < sites.txt
